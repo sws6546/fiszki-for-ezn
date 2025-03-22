58704d81a15c0a68b9e8855a9b25ee763aa3c02e
Your Name <you@example.com>
2025-03-22 11:49:20 +0100

    changet position of #

--- src/components/ResultsTable.tsx
+++ src/components/ResultsTable.tsx
@@ -7,44 +7,50 @@ export default function ResultsTable({ corrects, incorrects }: { corrects: flash
   const cardsManager = useContext(CardContext)
   return (
     <section className="container-sm text-center mt-4">
-      <table className="table table-danger">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Słówko</th>
-            <th scope="col">Odpowiedź użytkownika</th>
-            <th scope="col">Poprawne tłumaczenie</th>
-          </tr>
-        </thead>
-        <tbody>
-          {incorrects.map((incorrectCard, idx) => {
-            return <tr>
-              <th scope="row">{idx}</th>
-              <td>{incorrectCard.firstLangName}</td>
-              <td className="text-danger fw-bold">{incorrectCard.secondLangName}</td>
-              <td>{cardsManager?.cards.find(card => card.id === incorrectCard.id)?.secondLangName}</td>
+      {
+        incorrects.length != 0 &&
+        <table className="table table-danger">
+          <thead>
+            <tr>
+              <th className="text-start" scope="col">#</th>
+              <th scope="col">Słówko</th>
+              <th scope="col">Odpowiedź użytkownika</th>
+              <th scope="col">Poprawne tłumaczenie</th>
             </tr>
-          })}
-        </tbody>
-      </table>
-      <table className="table table-success">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Słówko</th>
-            <th scope="col">Odpowiedź użytkownika</th>
-          </tr>
-        </thead>
-        <tbody>
-          {corrects.map((correctCard, idx) => (
-            <tr key={correctCard.id}>
-              <th scope="row">{idx + 1}</th>
-              <td>{correctCard.firstLangName}</td>
-              <td className="text-success fw-bold">{correctCard.secondLangName}</td>
+          </thead>
+          <tbody>
+            {incorrects.map((incorrectCard, idx) => {
+              return <tr>
+                <th className="text-start" scope="row">{idx}</th>
+                <td>{incorrectCard.firstLangName}</td>
+                <td className="text-danger fw-bold">{incorrectCard.secondLangName}</td>
+                <td>{cardsManager?.cards.find(card => card.id === incorrectCard.id)?.secondLangName}</td>
+              </tr>
+            })}
+          </tbody>
+        </table>
+      }
+      {
+        corrects.length != 0 &&
+        <table className="table table-success">
+          <thead>
+            <tr>
+              <th className="text-start" scope="col">#</th>
+              <th scope="col">Słówko</th>
+              <th scope="col">Odpowiedź użytkownika</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {corrects.map((correctCard, idx) => (
+              <tr key={correctCard.id}>
+                <th className="text-start" scope="row">{idx + 1}</th>
+                <td>{correctCard.firstLangName}</td>
+                <td className="text-success fw-bold">{correctCard.secondLangName}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      }
     </section>
   )
 }
